571ac78ee300187a66ab91a07bf131eb621ea953
Fran <franmiralles03@gmail.com>
2023-09-24 14:49:10 +0200

    Widgets e imagen fondo

---
+++ PythonScripts/englishTeacher.py
@@ -0,0 +1,14 @@
+#import openai as ai
+
+
+#ai.api_key = "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"
+#res = ai.Completion.create(engine="gpt-3.5-turbo", prompt="¿Qué es chatGPT?", max_tokens=2048)
+
+#print(res.choices[0].text)
+
+from PIL import Image
+import pytesseract as py
+
+ruta_imagen = "/home/fran/Escritorio/GIT/Scripts/PythonScripts/img.jpg"
+imagen_abierta = Image.open(ruta_imagen)
+print(py.image_to_string(imagen_abierta))
